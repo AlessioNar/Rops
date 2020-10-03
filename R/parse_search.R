d86147af1e86f44af0e6c54e1170942e87cb1489
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-03 18:17:26 +0200

    Added parse_search and documentation updated

---
+++ R/parse_search.R
@@ -0,0 +1,25 @@
+#' @title Parse json response into docdb codes of patents
+#'
+#' @description Parse json response into docdb codes of patents
+#'
+#' @param response access token generated using the create_access_token function
+#' @return character vector containing docdb codes of query response
+#' @examples \dontrun{parse_search(response)}
+#' @export parse_search
+#'
+
+
+parse_search <- function(response){
+
+        results<- response$`ops:world-patent-data`$`ops:biblio-search`$`ops:search-result`$`ops:publication-reference`
+
+        country <- lapply(results, function(x) x$`document-id`$country$`$`)
+
+        doc_number <- lapply(results, function(x) x$`document-id`$`doc-number`$`$`)
+
+        kind <- lapply(results, function(x) x$`document-id`$`kind`$`$`)
+
+        docdb_id<- paste0(country, doc_number, kind)
+
+        return(docdb_id)
+}
