480b27811634473495c3e4e5b308fe6befc1577a
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-03 17:54:14 +0200

    multiple query and with/without biblio functionalities added

--- R/get_query.R
+++ R/get_query.R
@@ -17,10 +17,13 @@
 
 
 get_query <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
-                          inventor = NA, citation = NA, pub_num = NA, cpc = NA, ipc = NA, from = NA, to = NA) {
-
-  baseurl <- "http://ops.epo.org/3.2/rest-services/published-data/search/biblio/?q="
-
+                      inventor = NA, citation = NA, pub_num = NA, cpc = NA, ipc = NA, from = NA, to = NA,
+                      merge_classification = FALSE, with_biblio = FALSE) {
+  if(with_biblio == TRUE){
+    baseurl <- "http://ops.epo.org/3.2/rest-services/published-data/search/biblio/?q="
+  } else {
+    baseurl <- "http://ops.epo.org/3.2/rest-services/published-data/search/?q="
+  }
   if(is.na(title) == FALSE) {
     title <- paste0("ti=", title)
   }
@@ -28,7 +31,13 @@ get_query <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
     abstract <- paste0("ab=", abstract)
   }
   if(is.na(titab) == FALSE){
-    titab <- paste0("ta=", titab)
+    if(length(titab) > 1){
+      titab <- paste(titab, collapse = " or ")
+      titab <- paste0("(", titab,")")
+      titab <- paste0("ta=", titab)
+    } else {
+      titab <- paste0("ta=", titab)
+    }
   }
   if(is.na(applicant) == FALSE){
     applicant <- paste0("pa=", applicant)
@@ -43,10 +52,23 @@ get_query <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
     pub_num <- paste0("pn=", pub_num)
   }
   if(is.na(cpc) == FALSE){
-    cpc <- paste0("cpc=", cpc)
+    if(length(cpc) > 1){
+      cpc <- paste(cpc, collapse = " or ")
+      cpc <- paste0("(", cpc,")")
+      cpc <- paste0("cpc=", cpc)
+    } else {
+      cpc <- paste0("cpc=", cpc)
+    }
   }
   if(is.na(ipc) == FALSE){
-    ipc <- paste0("ipc=", ipc)
+    if(length(ipc) > 1){
+      ipc <- paste(ipc, collapse = " or ")
+      ipc <- paste0("(", ipc,")")
+      ipc <- paste0("ipc=", ipc)
+    } else {
+      ipc <- paste0("ipc=", ipc)
+    }
+
   }
 
   if(is.na(from) == FALSE & is.na(to) == FALSE) {
@@ -55,8 +77,13 @@ get_query <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
     dates<-NA
   }
 
-
-  query<-c(title, abstract, titab, applicant, inventor, citation, pub_num, cpc, ipc, dates)
+  if(merge_classification == TRUE){
+    class <- paste(cpc, ipc, sep = " or ")
+    class <- paste0("(", class, ")")
+    query<-c(title, abstract, titab, applicant, inventor, citation, pub_num, dates, class)
+  } else {
+    query<-c(title, abstract, titab, applicant, inventor, citation, pub_num, dates, cpc, ipc)
+  }
 
   query<-query[!is.na(query)]
 
@@ -65,6 +92,8 @@ get_query <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
   query<-gsub("=", "%3D", query)
 
   query<-gsub(" ", "%20", query)
+
+  query <- gsub("/", "%2F", query)
 
   url<-paste0(baseurl, query)
 
