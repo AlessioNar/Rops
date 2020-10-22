d11adaf3f623ed9185a68c31c1a15031cd85f14e
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-22 15:22:25 +0200

    bug fixes and comments added

--- R/get_claims.R
+++ R/get_claims.R
@@ -14,28 +14,39 @@
 #' @export get_claims
 
 get_claims<-function(epodoc_id, type = NA, access_token){
-
+  # Create header and name it
   header <- c(paste('Bearer', access_token), "application/json")
   names(header) <- c('Authorization', "Accept")
+
   baseURL <- "https://ops.epo.org/3.2/rest-services/published-data/"
+
   if(type == "pub"){
+    # Search among publications
     url <- paste0(baseURL, "publication/epodoc/")
   }
 
   if(type == "app"){
+    # Search among applications
     url <- paste0(baseURL, "application/epodoc/")
   }
 
+  # Access through the claims endpoint
   request<-paste0(url, epodoc_id, "/claims")
 
+  # Make requests
   response<-GET(request,
                 add_headers(header))
 
+  # Parse json file into list
   parsed_response<-fromJSON(content(response, "text"), flatten = TRUE)
 
+  # Create df containing claims and language
   claims_df<-as.data.frame(parsed_response$`ops:world-patent-data`$`ftxt:fulltext-documents`$`ftxt:fulltext-document`$claims)
+
+  # Select only claims in english language
   claims<-claims_df$X.[which(claims_df$X.lang == "EN")]
 
+  # Remove webfonts
   claims<-gsub("\\n", " ", claims)
 
   return(claims)
