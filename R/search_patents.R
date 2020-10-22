cbe46de7c7c048c339da3b459ca38fc53234ad2e
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-22 15:58:29 +0200

    function search_patents created

---
+++ R/search_patents.R
@@ -0,0 +1,28 @@
+#' @title Search patents
+#' @description Search for patents contained in the EPO dataset
+#' @param query url built using the function get_query
+#' @param access_token access token received after authentication
+#' @return Dataframe containing patents
+#' @export search_patents
+#' @examples \dontrun{search_patents(query, access_token)}
+
+search_patents <- function(title = NA, abstract = NA, titab = NA, applicant = NA,
+                           inventor = NA, citation = NA, pub_num = NA, cpc = NA, ipc = NA,
+                           from = NA, to = NA, with_biblio = FALSE, access_token,
+                           fromRange = 1, toRange = 100){
+
+  # Create query
+  url <- get_query(title = title, abstract = abstract,
+                 titab = titab, applicant = applicant, inventor = inventor,
+                 pub_num = pub_num, cpc = cpc, ipc = ipc, from = from,
+                 to = to, with_biblio = with_biblio)
+
+  # Make request
+  search_results<- get_ops(url, access_token, raw = FALSE, fromRange = fromRange, toRange = toRange)
+
+  # Parse results
+  dataframe<- parse_search(search_results)
+
+  return(dataframe)
+
+}
