e0320dd25c4c2bc02a30b333cd230cb598a36df7
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-22 15:45:30 +0200

    bug fixes and comments added

--- R/get_result_number.R
+++ R/get_result_number.R
@@ -2,21 +2,26 @@
 #' @description This is a memory efficient way to count the number of results of a single query. Whenever it returns 10000, it means that the number of results are 10000 or higher
 #' @param query url built using the function get_query
 #' @param access_token access token received after authentication
-#' @return
+#' @return Number of results
 #' @export get_result_number
 #' @examples \dontrun{get_result_number(query, access_token)}
 #'
 
 get_result_number<- function(query, access_token){
-  #create header containing only one info
+
+  # Create header creating a range of 1
   header <- c(paste('Bearer', access_token), "application/json", paste0(1, "-", 1))
+
   # Rename header
   names(header) <- c('Authorization', "Accept", "X-OPS-Range")
-  #make request
+
+  # Make request
   response <- httr::GET(url = query, add_headers(header))
-  #extract content
+
+  # Extract content
   content <- content(response)
-  #retrieve result number
+
+  # Retrieve result number
   result_number <- content[["ops:world-patent-data"]][["ops:biblio-search"]][["@total-result-count"]]
 
   return(result_number)
