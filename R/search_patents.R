2462806ea8c16e5e9e6ecbfcc0c5bfef8e9715d5
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-22 16:08:40 +0200

    Bug fixes

--- R/search_patents.R
+++ R/search_patents.R
@@ -20,8 +20,13 @@ search_patents <- function(title = NA, abstract = NA, titab = NA, applicant = NA
   # Make request
   search_results<- get_ops(url, access_token, raw = FALSE, fromRange = fromRange, toRange = toRange)
 
+  if (with_biblio == FALSE){
   # Parse results
   dataframe<- parse_search(search_results)
+  } else{
+    # the function parse_search_biblio is unstable, I need to check it
+    dataframe <- parse_search_biblio(search_results)
+  }
 
   return(dataframe)
 
