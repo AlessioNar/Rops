671f7faf0e8f74e6c8421f25f02a91f20282f7a7
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-22 15:38:45 +0200

    Updated documentation and fix bugs

--- R/get_ops.R
+++ R/get_ops.R
@@ -3,7 +3,9 @@
 #' get request to retrieve the data from OPS. When the response is received extract the content to create a list.
 #' @param url . A single url
 #' @param access_token bearer token for authentication
-#' @param raw boolean value to
+#' @param raw boolean value that decides whether the returned file will be in json format or as a list
+#' @param from_range Initial limit of results to be returned. Default to 1
+#' @param to_range Final limit of results to be returned. Default to 100
 #' @return A list containing a parse
 #' @importFrom httr GET
 #' @importFrom httr content_type
@@ -12,18 +14,16 @@
 #' @export
 #' @examples \dontrun{search_respose <- get_ops("https://ops.epo.org/3.2/rest-services/published-data/search/?q=ct%3D CN104980769 %20 and %20 pd %3D %22 20180201%2020180203%22", access_token, raw = FALSE)}
 
-get_ops <- function(url, access_token, raw = NULL, from_range = NULL, to_range = NULL){
+get_ops <- function(url, access_token, raw = NULL, from_range = 1, to_range = 100){
+
   # Paste token to bearer. In this case it is possible to improve this function to decide the response,
   # either in xml or in json format.
-  if(is.null(from_range) == TRUE | is.null(to_range) == TRUE ){
-    header <- c(paste('Bearer', access_token), "application/json")
-    # Rename header
-    names(header) <- c('Authorization', "Accept")
-  } else {
+
+  # Create header
   header <- c(paste('Bearer', access_token), "application/json", paste0(from_range, "-", to_range))
   # Rename header
   names(header) <- c('Authorization', "Accept", "X-OPS-Range")
-  }
+
   #make request
   response <- httr::GET(url = url, add_headers(header))
 
@@ -36,5 +36,5 @@ get_ops <- function(url, access_token, raw = NULL, from_range = NULL, to_range =
 
   return(content)
   }
-  }
+}
 
