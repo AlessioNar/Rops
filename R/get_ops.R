a30ad892673d52132d8f159c6806468865111590
Alessio Nardin <alessio.nardin@gmail.com>
2022-09-13 23:38:50 +0200

    get_ops refactored

--- R/get_ops.R
+++ R/get_ops.R
@@ -1,6 +1,6 @@
-#' @title Obtain a set of urls from ops_publications
-#' @description Used in conjunction with ops_iterate. Given a set of urls convert to a
-#' get request to retrieve the data from OPS. When the response is received extract the content to create a list.
+#' @title Makes a request to the Open Patent Services API
+#' @description Makes a request to the Open Patent Services API
+#' and it returns the raw or parsed response
 #' @param url string, A single url
 #' @param access_token string, bearer token for authentication
 #' @param raw bool, r
@@ -11,26 +11,32 @@
 #' @importFrom httr content_type
 #' @importFrom httr accept
 #' @importFrom httr content
-#' @importFrom httr header
+#' @importFrom httr add_headers
+#' @importFrom httr status_code
+#' @importFrom httr headers
 #' @export
 #' @examples \dontrun{
-#' search_respose <- get_ops("https://ops.epo.org/3.2/rest-services/published-data/search/?q=ct%3DCN104980769%20and%20pd%3D%2220180201%2020180203%22", access_token, raw = FALSE)
+#' search_respose <- get_ops(url, access_token, raw = FALSE)
 #' }
-#' 
+#'
 
 get_ops <- function(url, access_token, raw = NULL) {
-  
+
   # Create header
-  header <- c(paste('Bearer', access_token), "application/json")
+  header <- c(paste("Bearer", access_token), "application/json")
   # Rename header
-  names(header) <- c('Authorization', "Accept")
+  names(header) <- c("Authorization", "Accept")
   # Make request
   response <- httr::GET(url = url, httr::add_headers(header))
-  
+
   # Check if the response is 200
   if (httr::status_code(response) != 200) {
-    # Stop the function if the response is not 200 and return the error code and headers
-    stop(paste0(httr::status_code(response), "/n", as.character(httr::headers(response))))
+    # Stop the function if the response is not 200 and
+    # return the error code and headers
+    stop(
+      paste0(httr::status_code(response),
+      "/n",
+      as.character(httr::headers(response))))
   } else {
     # Convert the json file into a list
     if (raw == TRUE) {
