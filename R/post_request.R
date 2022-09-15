a698dcba16851a7339db0f5393ce53e10ecc98a8
Alessio Nardin <alessio.nardin@gmail.com>
2022-09-15 10:06:29 +0200

    implemented post request!

--- R/post_request.R
+++ R/post_request.R
@@ -1,41 +1,37 @@
-#' @title Makes a request to the Open Patent Services API
-#' @description Makes a request to the Open Patent Services API
+#' @title Makes a POST request to the Open Patent Services API
+#' @description Makes a POST request to the Open Patent Services API
 #' and it returns the raw or parsed response
 #' @param url string, A single url
 #' @param access_token string, bearer token for authentication
 #' @param raw bool, r
 #' @return A list
-#' @importFrom httr GET
 #' @importFrom httr content_type
 #' @importFrom httr accept
 #' @importFrom httr content
 #' @importFrom httr add_headers
 #' @importFrom httr status_code
 #' @importFrom httr headers
-#' @export
+#' @export post_request
 #' @examples \dontrun{
 #' search_respose <- get_ops(url, access_token, raw = FALSE)
 #' }
 #'
 
-get_ops <- function(url, access_token, raw = NULL, multiple=FALSE, id=NULL) {
+post_request <- function(url, access_token, raw = raw, id = NULL) {
+  
+  request_body <- stringr::str_c(paste(id, collapse = ","))
 
-  # Create header
   headers <- c(paste("Bearer", access_token), "application/json")
   # Rename header
   names(headers) <- c("Authorization", "Accept")
 
-  if(multiple == TRUE){
-    
-    headers  <- c(headers, paste(id))
-    names(headers) <- c("Authorization", "Accept", "Request Body")
-    # Make a POST request
-    response <- httr::POST(url, httr::add_headers(headers = headers))
-  } else {
-    # Make a GET request
-    response <- httr::GET(url = url, httr::add_headers(headers))
-  }
-  
+  # Make the POST request
+  response <- httr::POST(url,
+                      body = request_body,
+                      encode = "raw",
+                      content_type("text/plain"),
+                      add_headers(headers)
+                      )
 
   # Check if the response is 200
   if (httr::status_code(response) != 200) {
@@ -44,7 +40,7 @@ get_ops <- function(url, access_token, raw = NULL, multiple=FALSE, id=NULL) {
     stop(
       paste0(httr::status_code(response),
       "/n",
-      as.character(httr::headers(response))))
+      httr::headers(response)))
   } else {
     # Convert the json file into a list
     if (raw == TRUE) {
--- R/publication_services.R
+++ R/publication_services.R
@@ -1,42 +1,36 @@
 #' @title Queries the Publication Services Open Patent Services API
 #'
-#' @description Retrieve claims of a patent. It works only with the epodoc format
+#' @description Retrieve claims of a patent.
+#' It works only with the epodoc format
 #'
-#' @param epodoc_id character, epodoc code for a patent to have returned the claims
+#' @param id string or string vector, epodoc or docdb format
+#' @param type the type of publication to be searched.
+#' 'publication' or 'application'.
+#' @param format the format of the document id to be searched.
+#' 'epodoc' or 'docdb'.
 #' @param access_token token for authentications
-#' @param type the type of publication to be searched. 'pub' stands for patent publications and 'app' stands for patent applications.
-#' @return A character vector containing patent claims
-#'
-#' @examples \dontrun{get_full_text(epodoc_id, type = 'pub', access_token))}
-#' @import httr
+#' @param raw bool, if TRUE returns the raw response
+#' @param what string, the type of information to be retrieved.
+#' 'claims', 'abstract', 'description', 'title', 'fulltext', 'full-cycle'
+#' @return A list or a raw string
+#' @examples \dontrun{publication_services(id = "EP1000000.A1", type = "publication", format = "epodoc", access_token = "your_access_token", raw = FALSE, what = "claims")}
+#' @import stringr
 #' @export publication_services
 
 publication_services<-function(id, type, format, access_token, what = "fulltext", raw = FALSE){
 
-    baseURL <- "http://ops.epo.org/3.2/rest-services/published-data"
+    baseURL <- "https://ops.epo.org/3.2/rest-services/published-data"
 
-    if(is.vector(id) == TRUE){
-            # Paste ids together
-            id<-paste0(id, collapse = ", ")
-    }
-
-    
-    url <- stringr::str_c(baseURL, "/", type, "/", format, "/", id, "/", what)
-    
-    
-    print(url)
-    # If clause to check if id is a vector or not, if it is, use post_request if it is not, use get_request
-    if(is.vector(id) == TRUE){
-        response <- get_ops(url, access_token, raw = raw, multiple = TRUE, id = id)
+    # If clause to check if the user supplied a string or a string array
+    # if it is, use post_request if it is not, use get_request
+    if(length(id) == 1){
+        url <- stringr::str_c(baseURL, "/", type, "/", format, "/", id, "/", what)
+        response <- get_request(url, access_token, raw = raw)
     } else {
-        response <- get_ops(url, access_token, raw = raw)
+        url <- stringr::str_c(baseURL, "/", type, "/", format, "/", what)
+        response <- post_request(url, access_token, raw = raw, id = id)
     }
 
-
-
-
-    # Make request
-    response<-get_ops(url, access_token, raw = raw)
     # return the response
     return(response)
 
